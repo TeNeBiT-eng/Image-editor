e572f88e8b4cb3706f13b3edf98aa556c43c1688
Tomisin Babalola <ademolababalola2003@gmail.com>
2022-08-05 16:03:00 +0100

    reset filter function

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const filterOptions = document.querySelectorAll(".filter button");
 const filterName = document.querySelector(".filter-info .name");
 const filterValue = document.querySelector(".filter-info .value");
 const filterSlider = document.querySelector(".slider input");
-const rotateOptions = document.querySelectorAll('.rotate button')
+const rotateOptions = document.querySelectorAll(".rotate button");
 const previewImg = document.querySelector(".preview-img img");
+const resetFilterBtn = document.querySelector(".reset-filter");
 const chooseImgBtn = document.getElementById("choose");
 
 let brightness = 100,
@@ -12,10 +13,12 @@ let brightness = 100,
   inversion = 0,
   grayscale = 0;
 
-let rotate = 0, flipHorizontal = 1, flipVertical = 1;
+let rotate = 0,
+  flipHorizontal = 1,
+  flipVertical = 1;
 
 const applyFilter = () => {
-    previewImg.style.transform = `rotate(${rotate}deg) scale(${flipHorizontal}, ${flipVertical})`;
+  previewImg.style.transform = `rotate(${rotate}deg) scale(${flipHorizontal}, ${flipVertical})`;
   previewImg.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
 };
 
@@ -70,24 +73,37 @@ const updateFilter = () => {
   applyFilter();
 };
 
-rotateOptions.forEach(option => {
-    option.addEventListener('click', () => {
-        if(option.id === 'left') {
-            rotate -= 90 // if left rotate btn is clicked, decrement by rotate value by -90
-        } else if(option.id === 'right') {
-            rotate += 90 // if right rotate btn is clicked, increment by rotate value by +90
-        }
-        else if(option.id === 'horizontal') {
-            // if flipHorizontal value is 1, set this value to -1 else set to 1
-            flipHorizontal = flipHorizontal === 1 ? -1 : 1;
-        } else {
-            // if flipVertical value is 1, set this value to -1 else set to 1
-            flipVertical = flipVertical === 1 ? -1 : 1;
-        }
-        applyFilter();
-    })
+rotateOptions.forEach((option) => {
+  option.addEventListener("click", () => {
+    if (option.id === "left") {
+      rotate -= 90; // if left rotate btn is clicked, decrement by rotate value by -90
+    } else if (option.id === "right") {
+      rotate += 90; // if right rotate btn is clicked, increment by rotate value by +90
+    } else if (option.id === "horizontal") {
+      // if flipHorizontal value is 1, set this value to -1 else set to 1
+      flipHorizontal = flipHorizontal === 1 ? -1 : 1;
+    } else {
+      // if flipVertical value is 1, set this value to -1 else set to 1
+      flipVertical = flipVertical === 1 ? -1 : 1;
+    }
+    applyFilter();
+  });
 });
+
+const resetFilter = () => {
+  // resetting all variable to default value
+  brightness = 100;
+  saturation = 100;
+  inversion = 0;
+  grayscale = 0;
+  rotate = 0;
+  flipHorizontal = 1;
+  flipVertical = 1;
+  filterOptions[0].click(); //clicking brightness btn, so the brightness is selected by default
+  applyFilter();
+};
 
 fileInput.addEventListener("change", loadImage);
 filterSlider.addEventListener("input", updateFilter);
+resetFilterBtn.addEventListener("click", resetFilter);
 chooseImgBtn.addEventListener("click", () => fileInput.click());
